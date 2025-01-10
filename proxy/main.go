1bf3ddd2ea885e477b93491113b502b73c708077
Zhodaran <goidenko_ctac@mail.ru>
2025-01-10 23:59:55 +0300

    SQL struct table

--- proxy/main.go
+++ proxy/main.go
@@ -87,6 +87,8 @@ func main() {
 	fmt.Println("DB_NAME:", os.Getenv("DB_NAME"))
 	fmt.Println("DB_HOST:", os.Getenv("DB_HOST"))
 	fmt.Println("DB_PORT:", os.Getenv("DB_PORT"))
+	fmt.Println("Запуск задержки")
+	time.Sleep(10 * time.Second)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
